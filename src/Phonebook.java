b2ceb7b367a67b606427f715c4a55026712b3225
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-10 16:05:51 +0200

    Es3 class Phonebook

---
+++ src/Phonebook.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Phonebook {
+
+    public HashMap<String, String> phonebook;
+
+    public void add(String name, String number) {
+        this.phonebook.put(name, number);
+    }
+
+    public void remove(String name) {
+        this.phonebook.remove(name);
+    }
+
+    public String findNmae(String number) {
+        for (Map.Entry<String, String> entry : this.phonebook.entrySet()) {
+            if (entry.getValue().equals(number)) {
+                return entry.getKey();
+            }
+        }
+        return "not found";
+    }
+
+    public String findNumber(String name) {
+        if (this.phonebook.get(name) != null) return this.phonebook.get(name);
+        else return "not found";
+    }
+
+    public void printAll() {
+        for (Map.Entry<String, String> entry : this.phonebook.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
